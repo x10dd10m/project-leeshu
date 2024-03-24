bbd61c3f19f1941ae8b266a2a21aed4c1564e7e6
x10dd10m <cool.coolsam60@gmail.com>
2024-03-25 03:21:02 +0500

    initial commit

---
+++ msg.js
@@ -0,0 +1,15 @@
+// for my baby girl alishbah
+// from saddam
+
+function submitForm() {
+
+    var user_input = document.getElementById("floatingInput").value.toString();
+    var display_msg = document.getElementById("display-answer");
+
+    if (user_input.toLowerCase() == "alishbah"){
+        display_msg.innerHTML = "Yess Alishbah, You're Saddam's WHOLE HEART and he LOVEEES YOU SOOOOO MUCHHHH!!!"
+    }
+    else{
+        display_msg.innerHTML = "Ummm, Nopee"
+    }
+}
